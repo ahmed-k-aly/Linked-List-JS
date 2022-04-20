b206a96fb1cf67fa79b66bc97d71b530648d59ff
ahmed-k-aly <ahmedkaly1101@gmail.com>
2022-04-20 10:51:25 -0400

    Create remove method.

--- Linked-List.js
+++ Linked-List.js
@@ -12,6 +12,11 @@ class LinkedList{
         this.head = null;
         this.size = 0;
     }
+    /**
+     * @brief adds the passed in element to the beginning of the linked list.
+     * @timeComplexity O(1)
+     * @param {*} _value the passed in value to add 
+     */
     insert(_value){
         let newNode = new Node(_value);
         if (head === null){
@@ -26,5 +31,38 @@ class LinkedList{
         this.head = newNode;
         this.size++;
     }
-    
+    /**
+     * @brief Removes the first occurrence of a node with the passed in value from the LinkedList.
+     * @param {E} _value the value to remove from the LinkedList.
+     * @returns a boolean indicating whether the removal was successful or not.
+     */
+    remove(_value){
+        let current = this.head;
+        if (this.head === null){
+            // no nodes to remove in the linked list.
+            return false;
+        }
+        if (this.head.value === _value){
+            // if we are remove the head node, then just
+            // update the head pointer
+            this.head = head.next;
+            this.size--;
+            return true;
+        }
+        while (current.next!=null){
+            // traverse the LL.
+            if (current.next.value === _value){
+                // if the next node is the one to remove, then
+                // make out current node point to its next;
+                current.next = current.next.next;
+                this.size--;
+                return true;
+            }
+            // update current pointer.
+            current = current.next;
+        }
+        return false;
+    }
+
+
 }
