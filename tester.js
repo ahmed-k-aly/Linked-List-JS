a92b0ecb8a2313aefd8611868231710329ff42e6
ahmed-k-aly <ahmedkaly1101@gmail.com>
2022-04-22 16:04:32 -0400

    testing concluded.

--- tester.js
+++ tester.js
@@ -2,11 +2,15 @@ import LinkedList from './Linked-List.js';
 main();
 
 function main(){
-    let isSuccessful = addTest();
-     if (!isSuccessful) {
+    
+    let isAddSuccessful = addTest();
+     if (!isAddSuccessful) {
          console.log("Add test failed.");
      } else console.log("Add test succeeded.");
-
+    let isRemoveSuccessful = removeTest();
+    if (!isRemoveSuccessful) {
+        console.log("Remove test failed.");
+    } else console.log("Remove test succeeded.");
 }
 
 /**
@@ -15,18 +19,59 @@ function main(){
  * @returns 
  */
 function addTest(){
-    let elements = [];
-    let ll = new LinkedList();
-    let arrayLength = Math.floor((1+Math.random()) * 100);
-    for (let i = 0; i < arrayLength; i++) {
-        elements[i] = i;
-        ll.insert(i);
-    }
-    for (let i = 0; i < arrayLength; i++) {
-        if (ll.contains(elements[i])){
-            continue;
+    for (let _ = 0; _< 100; _++) {
+        let elements = [];
+        let ll = new LinkedList();
+        let arrayLength = Math.floor((1+Math.random()) * 100);
+        for (let i = 0; i < arrayLength; i++) {
+            elements[i] = i;
+            ll.insert(i);
         }
-        return false;
+        for (let i = 0; i < arrayLength; i++) {
+            if (ll.contains(elements[i])){
+                continue;
+            }
+            return false;
+        }
     }
     return true;
+}
+
+function removeTest(){
+    let ll = new LinkedList();
+    for (let _ = 0; _ <100; _++){
+        let removeNull = ll.remove(10);
+        if (removeNull) return false;
+        let numElements = Math.floor(1+(Math.random() * 100));
+        for (let i = 0; i < numElements; i++) {
+            // populate the linked list.
+            ll.insert(i);
+        }
+        if (ll.size() != numElements) {
+            console.log("size method failed");
+            return false;
+        }
+        while (ll.size() > 0) {
+            // choose a random element to remove from the list
+            let numToRemove = Math.floor((Math.random()) * numElements);
+            let isNumInList = ll.contains(numToRemove);
+            let isNumRemoved = ll.remove(numToRemove);
+            if (isNumRemoved != isNumInList){
+                return false;
+            }
+        }
+        for (let i = 0; i < numElements; i++){
+            // populate the linked list with one value.
+            ll.insert(0);
+        }
+        while (ll.size() > 0) {
+            // empt the linked list
+            let isNumRemoved = ll.remove(0);
+            if (!isNumRemoved){
+                return false;
+            }
+        }
+    }
+    return true;
+
 }
